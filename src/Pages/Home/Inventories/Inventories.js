5ac734b0e23fc6db1682fb05e05c74ceaf872594
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-04 00:37:45 -0700

    image set in card and card set on home page

--- src/Pages/Home/Inventories/Inventories.js
+++ src/Pages/Home/Inventories/Inventories.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Inventory from '../../Inventory/Inventory';
-
+import './Inventories.css';
 
 const Inventories = () => {
     const [inventories, setInventories] = useState([]);
@@ -11,14 +11,16 @@ const Inventories = () => {
             .then(data => setInventories(data));
     }, [])
     return (
-        <div>
-            <h2>Inventories:{inventories.length}</h2>
-            {
-                inventories.map(inventory => <Inventory
-                    key={inventory.id}
-                    inventory={inventory}>
-                </Inventory>)
-            }
+        <div >
+            <h1>Warehouse Inventories</h1>
+            <div className='row row-cols-1  row-cols-md-2 row-cols-lg-3 g-4 text-center'>
+                {
+                    inventories.map(inventory => <Inventory
+                        key={inventory.id}
+                        inventory={inventory}>
+                    </Inventory>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -4,7 +4,18 @@ const Inventory = ({ inventory }) => {
     const { product, image, supplier, quantity, price, description } = inventory;
     return (
         <div>
-            <img src={image} alt=""></img>
+
+            <div className="card ">
+                <div className='text-center'>
+                    <img style={{ width: "200px", height: "200px", objectFit: "cover" }} src={image} className="card-img-top " alt="..." />
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+
+            </div>
+            {/* <img src={image} alt=""></img>
             <h2>{product}</h2>
             <p>price: {price}</p>
             <p>Supplier name: {supplier}</p>
@@ -12,6 +23,8 @@ const Inventory = ({ inventory }) => {
             <p><small>
                 Description:{description}
             </small></p>
+            <button>Manage Stock</button> */}
+
 
         </div>
     );
