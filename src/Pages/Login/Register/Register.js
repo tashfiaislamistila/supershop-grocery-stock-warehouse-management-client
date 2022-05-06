2086287ad67933bf68d2a61cf1307deffbeeb699
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-06 14:50:50 -0700

    register functionality

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,41 +1,35 @@
-import { Button } from 'bootstrap';
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
+
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+
+    }
     return (
-        <div className='container w-50 mx-auto border mt-5 mb-5 shadow-lg rounded'>
-            <div className='mt-5 text-center mb-5'>
-                <h2>Register</h2>
-            </div>
-            <Form >
-                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicEmail">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" required />
-                </Form.Group>
+        <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
 
-                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
-                </Form.Group>
-                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-            </Form >
-            <div className='mb-5  p-2 mx-3'>
-                <Button variant="primary" type="submit">
-                    Register
-                </Button>
-            </div>
-
+                <input type="email" name="email" id="" placeholder='Your Email'
+                    required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account?<Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}> Please Login</Link></p>
         </div >
     );
 };
