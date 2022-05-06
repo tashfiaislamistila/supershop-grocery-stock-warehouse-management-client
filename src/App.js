bf2c7de82139400ee257752bae3787c4a785995c
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-06 13:46:54 -0700

    Login functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Inventories from './Pages/Home/Inventories/Inventories';
 import Inventory from './Pages/Inventory/Inventory';
 import InventoryDetail from './Pages/InventoryDetail/InventoryDetail';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import ManageInventories from './Pages/ManageInventories/ManageInventories';
 import ManageInventory from './Pages/ManageInventory/ManageInventory';
 import Footer from './Pages/Shared/Footer/Footer';
@@ -28,6 +29,7 @@ function App() {
         <Route path="/inventories" element={<Inventories></Inventories>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes >
       <Footer></Footer>
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+    }
+    const navigateToRegisterPage = event => {
+        navigate('/register');
+    }
     return (
-        <div>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+        <div className='container w-50 mx-auto border mt-5 mb-5 shadow-lg rounded'>
+            <div className='mt-5 text-center mb-5'>
+                <h1>Please Login</h1>
+            </div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Group className="mb-3  p-2 mx-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <div className='mb-5  p-2 mx-3'>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </div>
             </Form>
+            <div className='mb-4 px-4 py-2 '>
+                <p>New to Supershop Grocery Stock?<Link to="/register" className='text-danger pe-auto text-decoration-none ' onClick={navigateToRegisterPage}> Please Register</Link></p>
+            </div>
         </div>
     );
 };
