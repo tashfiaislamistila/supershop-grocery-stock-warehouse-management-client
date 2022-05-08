f5b74d572b1d0bc8577bfc5626423de73ad80833
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-08 05:33:54 -0700

    jwt

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -7,6 +7,7 @@ import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -29,18 +30,21 @@ const Login = () => {
     }
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
     if (error) {
         errorElement = <p className='text-danger'>Error: {error?.message} </p>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('access Token', data.accessToken);
+        navigate(from, { replace: true });
     }
     const navigateToRegisterPage = event => {
         navigate('/register');
